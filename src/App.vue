cd771b9935dfb1c083d13fc7e985e325c45765d0
Diego Urçulino <diegourculino@hotmail.com>
2024-07-10 22:58:10 -0300

    feat: Adicionado modal de novo knight

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,118 @@
 <template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <div id="app" class="container my-3">
-    <h3>Innit Vue</h3>
 
-    <div class="card mt-3">
-      <div class="card-header">Vue Axios GET</div>
-      <div class="card-body">
-        <div class="input-group input-group-sm">
-          <button class="btn btn-sm btn-primary" @click="getAllData">Get All</button>
-
-          <button class="btn btn-sm btn-warning ml-2" @click="clearGetOutput">Clear</button>
-        </div>   
-        
-        <div v-if="getResult" class="alert alert-secondary mt-2" role="alert"><pre>{{getResult}}</pre></div>
+ 
+      <div class="row">
+        <div class="col">
+          <img width="220" src="./assets/logo.png">
+        </div>
+        <div class="col">
+          <h4>Diego Urçulino de Sousa</h4>
+          <p>Teste Técnico - BTG Pactual</p>
+        </div>
       </div>
-    </div>
+
+
+      <div class="row acoes-header">
+        <div class="col">
+          <button type="button" class="btn btn-success" @click="showModal = true">
+            <i class="bi bi-plus-circle"></i>
+            Adicionar Knights
+          </button>
+        </div>
+        <div class="col d-flex flex-row-reverse">
+          <button type="button" class="btn btn-warning">
+            Hall Of Heroes
+            <i class="bi bi-star-fill"></i>
+          </button>
+        </div>
+      </div>
+
+      <div v-if="knightSaveSucess" class="alert alert-success alert-dismissible fade show margin-alert" role="alert">
+        <strong>Sucesso!</strong> Cavaleiro salvo.
+        <button @click="knightSaveSucess = false" type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div v-if="loadListKnights" class="loader">
+          <div class="spinner-grow text-primary margin-loader" v-for="index in contentLoad" :key="index" role="status"></div>
+      </div>
+
+      <div v-if="loadListKnights == false && knightsResult == null" class="cards-render">
+        <div class="card margin-card">
+          <h3 class="h3-padding">Nenhum knight cadastrado</h3>
+        </div>
+      </div>
+
+      <div v-if="loadListKnights == false && knightsResult != null" class="cards-render">
+        <div class="card margin-card" v-for="(knight, index) in knightsResult" :key="index">
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">{{ knight.name }}</h5>
+                <span class="badge badge-primary padding-badge">
+                  <i class="bi bi-person-fill"></i>
+                  Idade: 25
+                </span>
+                <span class="badge badge-dark padding-badge">
+                  <i class="bi bi-shield-fill"></i>
+                  Armas: 2
+                </span>
+              </div>
+              <div class="col d-flex flex-row-reverse">
+                <div row>
+                  <button type="button" class="btn btn-primary btn-lg" @click="showModal = true">
+                    <i class="bi bi-pencil-square"></i>
+                  </button>
+                  <button type="button" class="btn btn-danger btn-lg margin-buttons" @click="showModal = true">
+                    <i class="bi bi-trash"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal -->
+      <modal
+        @submitKnight="knightSubmission"
+        :show="showModal" :salvando="salvando" @close="showModal = false" @save="saveKnight">
+      </modal>
  
   </div>
 </template>
 
 <script>
 import http from "./http-common";
+import Modal from './NewKnight.vue';
 
 export default {
   name: "App",
   data() {
     return {
-      getResult: null
+      contentLoad: null,
+      loadListKnights: false,
+      showModal: false,
+      knightsResult: null,
+      salvando: false,
+      knightSaveSucess: false
     }
   },
+  components: {
+    Modal
+  },
   methods: {
-    fortmatResponse(res) {
-      return JSON.stringify(res, null, 2);
-    },
-
-    async getAllData() {
+    async getAllKnights() {
+      this.contentLoad = 10;
+      this.loadListKnights = true;
       try {
+        setTimeout(() => {
+          this.loadListKnights = false;
+        }, "2000");
         const res = await http.get("/knights");
 
         const result = {
@@ -43,22 +121,79 @@ export default {
           data: res.data,
         };
 
-        this.getResult = this.fortmatResponse(result);
+        this.knightsResult = null;
+        this.knightsResult = result.data;
       } catch (err) {
-        this.getResult = this.fortmatResponse(err.response?.data) || err;
+        this.knightsResult = this.fortmatResponse(err.response?.data) || err;
       }
     },
 
-    clearGetOutput() {
-      this.getResult = null;
+    knightSubmission(newKnight) {
+      console.log(newKnight);
+      this.salvando = true;
+
+      setTimeout(() => {
+        this.salvando = false;
+        this.showModal = false;
+        this.knightSaveSucess = true;
+        this.getAllKnights();
+      }, "2000");
     },
+
+    async saveKnight(){
+
+    }
+
+  },
+  beforeMount() {
+    this.getAllKnights()
   }
 }
 </script>
 
 <style>
 #app {
-  max-width: 600px;
+  max-width: 100vw;
   margin: auto;
 }
+h4, p{
+  text-align: end;
+  padding-right: 20px;
+}
+
+.acoes-header{
+  padding: 30px 20px;
+}
+
+.margin-card{
+  margin: 0px 20px 30px 20px;
+}
+
+.margin-buttons{
+  margin: 0px 20px;
+}
+.margin-alert{
+  margin: 0px 20px;
+}
+.h3-padding{
+  padding: 20px;
+}
+
+.loader{
+  width: 100%;
+  text-align: center;
+}
+
+.margin-loader{
+  margin: 20px 10px;
+}
+.padding-badge{
+  i{
+    margin-right: 5px;
+  }
+  padding: 10px !important;
+  margin-left: 8px;
+  font-weight: normal !important;
+}
+
 </style>
